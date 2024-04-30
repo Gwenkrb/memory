e851cb4430c9aa5b08ff7ee6078611ae09167e0e
Gwenstard <gwen.kerbastard@gmail.com>
2024-04-30 11:38:29 +0200

    Création de l'animation retournement de cartes en JS et CSS

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
-// Le jeu sera dans la div class : board
-// 
+// La constante cards stocke toutes les cartes avec la div "memory-card"
+const cards = document.querySelectorAll('.memory-card');
+// La fonction flipCard n'a pas de paramètres. Elle rajoute flip quand on clique sur une carte.
+// Le nom de classe "memory-card" deviendra ainsi "memory-card flip" après l'événement this
+function flipCard() {
+    this.classList.toggle('flip');
+}
+// forEach parcourt toutes les div avec la classe memory-card et ajoute la classe flip à memory-card
+// Ce qui permet d'utiliser en CSS .memory-card.flip destiné au comportement visuel de la carte quand on clique dessus.
+cards.forEach(card => card.addEventListener('click', flipCard))
