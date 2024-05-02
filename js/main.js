e365676392cbd6855aa20611d98ae65e5d3cf72e
Gwenstard <gwen.kerbastard@gmail.com>
2024-05-02 17:04:33 +0200

    Refonte de tout le style et du CSS

--- js/main.js
+++ js/main.js
@@ -1,16 +1,3 @@
-// V1 de la fonction flipCard()
-
-// // La constante cards stocke toutes les cartes avec la div "memory-card"
-// const cards = document.querySelectorAll('.memory-card');
-// // La fonction flipCard n'a pas de paramètres. Elle rajoute flip quand on clique sur une carte.
-// // Le nom de classe "memory-card" deviendra ainsi "memory-card flip" après l'événement this
-// function flipCard() {
-//     this.classList.toggle('flip');
-// }
-// // forEach parcourt toutes les div avec la classe memory-card et ajoute la classe flip à memory-card
-// // Ce qui permet d'utiliser en CSS .memory-card.flip destiné au comportement visuel de la carte quand on clique dessus.
-// cards.forEach(card => card.addEventListener('click', flipCard))
-
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
@@ -34,11 +21,14 @@ function flipCard() {
         hasFlippedCard = true;
         firstCard = this;
         cptCard++;
+        let cptMessage = document.getElementById('cptMessage');
+        cptMessage.innerHTML = cptCard + " déplacements."
         console.log("Vous avez retourné ", cptCard, " cartes.");
         return;
     }
     secondCard = this;
     cptCard++;
+    cptMessage.innerHTML = cptCard + " déplacements."
     console.log("Vous avez retourné ", cptCard, " cartes.");
     checkForMatch();
     if(cptPaires===6){ 
